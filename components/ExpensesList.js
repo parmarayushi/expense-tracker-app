8ddeb380ccd69eb7a23ef447963789893a1285e9
Ayushi Parmar <ayushi.parmar@1rivet.com>
2024-03-13 16:27:16 +0530

    feat: styling for expenseListItems

--- components/ExpensesList.js
+++ components/ExpensesList.js
@@ -1,7 +1,8 @@
-import { FlatList, StyleSheet, Text } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData) {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
 }
 
 export default function ExpensesList({ expenses }) {
--- components/ExpensesOutput.js
+++ components/ExpensesOutput.js
@@ -28,6 +28,42 @@ const Expenses = [
     amount: 25.45,
     date: new Date("2020-1-15"),
   },
+  {
+    id: "e5",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
+  {
+    id: "e6",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
+  {
+    id: "e7",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
+  {
+    id: "e8",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
+  {
+    id: "e9",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
+  {
+    id: "e10",
+    description: "books",
+    amount: 25.45,
+    date: new Date("2020-1-15"),
+  },
 ];
 
 export default function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -43,6 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary200,
   },
 });
